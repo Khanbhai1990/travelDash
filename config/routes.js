a23f4bf670ad6b4f572047537f17b0a42e0ad9d2
Khanbhai1990 <hykhan1@asu.edu>
2017-10-25 11:18:32 -0700

    logic done

--- config/routes.js
+++ config/routes.js
@@ -10,12 +10,33 @@ module.exports = function(app){
     app.post('/users/login', users.check);
     app.post("/users/register", users.register);
 
-    app.get("/trips", trips.index);
-    app.post('/trips', trips.submit);
-
     app.get('/airline_login', airline.login);
     app.post('/airline_login', airline.check);
 
+//everything below this is protected
+    app.use(userAuth);
+
+    app.get("/trips", trips.index);
+    app.post('/trips', trips.submit);
+
+    app.use(airlineAuth);
+
     app.get('/air_main', air_main.index);
     app.post('/air_main', air_main.submit);
+}
+
+function userAuth(req, res, next){
+  if(req.session.user || req.session.airline){
+    next();
+  }else{
+    res.redirect('/users/login');
+  }
+}
+
+function airlineAuth(req, res, next){
+  if(req.session.airline){
+    next();
+  }else{
+    res.redirect('/airline_login');
+  }
 }
